0ebc172c63c6a88005b39f78d829bce7e78e652a
arum puri pratamawati <103489875+arumpuri@users.noreply.github.com>
2024-03-05 16:17:26 +0700

    adding router, delete app.jsx, update package json

--- shopping-cart/src/main.jsx
+++ shopping-cart/src/main.jsx
@@ -1,10 +1,31 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link,
+} from "react-router-dom";
+import Home from "./pages/Home"
+import About from "./pages/About"
+import Products from "./pages/Products"
+import ProductDetail from "./pages/productDetail"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+  <header>
+        <Link className="site-logo" to="/">#Shoplife</Link>
+        <nav>
+          <Link to="/about">About</Link>
+          <Link to="/products">Product</Link>
+        </nav>
+      </header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/products/:id" element={<ProductDetail />} />
+      </Routes>
+  </BrowserRouter>,
 )
